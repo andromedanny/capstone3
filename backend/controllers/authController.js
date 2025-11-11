68cb2fdf58d36b305737edcaf7778c60f2d7416f
andromedanny <prodanny80@gmail.com>
2025-11-12 04:27:28 +0800

    Add sequelize import and optimize database connection with faster timeouts

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -1,6 +1,7 @@
 import bcrypt from 'bcryptjs';
 import jwt from 'jsonwebtoken';
 import User from '../models/user.js';
+import sequelize from '../config/db.js';
 
 export const register = async (req, res) => {
   const startTime = Date.now();
